c29e2895d7fd84bf994822350cfdb3d84c52b6f1
Marcel Wiget <mwiget@gmail.com>
2024-07-30 11:12:43 +0200

    disabled decoding of token map due to occasional input error

--- site/token.tf
+++ site/token.tf
@@ -15,11 +15,9 @@ resource "restapi_object" "token" {
 
 output "token" {
   value = { 
-    content         = restapi_object.token.api_data
-    #content2        = jsondecode(restapi_object.token.api_data["spec"])
     content         = regex("content:(\\S+)", restapi_object.token.api_data.spec)[0]
     site_name       = regex("site_name:(\\S+)", restapi_object.token.api_data.spec)[0]
     token_algorithm = jsondecode(base64decode(split(".", regex("content:(\\S+)", restapi_object.token.api_data.spec)[0])[0]))
-    token_map       = jsondecode(base64decode(split(".", regex("content:(\\S+)", restapi_object.token.api_data.spec)[0])[1]))
+    #    token_map       = jsondecode(base64decode(split(".", regex("content:(\\S+)", restapi_object.token.api_data.spec)[0])[1]))
   }
 }
