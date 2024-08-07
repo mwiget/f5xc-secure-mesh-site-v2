b1c68ab60519b27956b2b870552783530e2932bb
Marcel Wiget <mwiget@gmail.com>
2024-08-07 10:43:50 +0200

    cleanup

--- output.tf
+++ output.tf
@@ -2,7 +2,7 @@ output "site" {
   value = {
     aws               = module.aws
     proxmox           = module.proxmox
-    #    vmware            = module.vmware
+    vmware            = module.vmware
     site-on-appstack  = module.site-on-appstack
   }
   sensitive = true
@@ -12,7 +12,7 @@ output "ip_address" {
   value = concat(
     module.aws[*].node.aws[*].ip_address, 
     module.proxmox[*].node.proxmox[*].ip_address, 
-    # module.vmware[*].node.vmware[*].ip_address,
+    module.vmware[*].node.vmware[*].ip_address,
     module.nutanix[*].node.nutanix[*].ip_address
   )
 }
